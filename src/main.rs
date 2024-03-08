3df6f7d3841f5c716a1d9106634276d1c9d387d6
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-03-07 22:47:52 -0500

    feat: can prepend prefix to className attribute

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,23 @@ use collect::CollectedClassNames;
 
 #[derive(Parser)]
 struct Cli {
-    css_file: PathBuf,
+    #[arg(short = 'i')]
+    css_input_file: PathBuf,
+
+    #[arg(long)]
+    prefix: String,
+
+    context: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    let c = CollectedClassNames::parse(cli.css_file)?;
+    let c = CollectedClassNames::parse(cli.css_input_file)?;
 
     c.report();
+
+    transform::prefix_classes(&cli.prefix, c.class_names)?;
 
     Ok(())
 }
@@ -21,13 +29,13 @@ fn main() -> anyhow::Result<()> {
 mod collect {
     use std::path::PathBuf;
 
-    use swc_core::ecma::utils::swc_common::SourceFile;
+    use swc_common::{FileName, SourceFile};
     use swc_css::visit::{Visit, VisitWith};
 
     use swc_css::{ast::Rule, parser::parse_file};
 
     pub struct CollectedClassNames {
-        class_names: Vec<String>,
+        pub class_names: Vec<String>,
     }
 
     impl CollectedClassNames {
@@ -46,14 +54,14 @@ mod collect {
 
             let options = swc_css::parser::parser::ParserConfig::default();
 
-            let filename = swc_core::ecma::utils::swc_common::FileName::Real(css_file);
+            let filename = FileName::Real(css_file);
 
             let cssfile = SourceFile::new_from(
                 filename.clone(),
                 false,
                 filename,
                 code.into(),
-                swc_core::ecma::utils::swc_common::BytePos(1),
+                swc_common::BytePos(1),
             );
 
             let mut errors = vec![];
@@ -93,34 +101,147 @@ mod collect {
     }
 
     impl Visit for CollectedClassNames {
-        fn visit_complex_selector(&mut self, n: &swc_css::ast::ComplexSelector) {
-            n.visit_children_with(self);
-        }
+        fn visit_compound_selector(&mut self, n: &swc_css::ast::CompoundSelector) {
+            let selectors = &n.subclass_selectors;
 
-        fn visit_complex_selector_children(&mut self, n: &swc_css::ast::ComplexSelectorChildren) {
-            match n {
-                swc_css::ast::ComplexSelectorChildren::CompoundSelector(selector) => {
-                    let selectors = &selector.subclass_selectors;
-
-                    selectors
-                        .iter()
-                        .filter_map(|s| match s {
-                            swc_css::ast::SubclassSelector::Class(selector) => Some(selector),
-                            _ => None,
-                        })
-                        .for_each(|s| {
-                            if s.text.value.contains(':') {
-                                let cn = s.text.value.split(':').last().unwrap();
-                                self.class_names.push(cn.to_string());
-                            } else {
-                                self.class_names.push(s.text.value.to_string());
-                            }
-                        });
-                }
-                swc_css::ast::ComplexSelectorChildren::Combinator(_) => {}
-            }
+            selectors
+                .iter()
+                .filter_map(|s| match s {
+                    swc_css::ast::SubclassSelector::Class(selector) => Some(selector),
+                    _ => None,
+                })
+                .for_each(|s| {
+                    if s.text.value.contains(':') {
+                        let cn = s.text.value.split(':').last().unwrap();
+                        self.class_names.push(cn.to_string());
+                    } else {
+                        self.class_names.push(s.text.value.to_string());
+                    }
+                });
         }
     }
 }
 
-mod transform {}
+mod transform {
+    use anyhow::Context;
+    use std::path::Path;
+    use swc_atoms::Atom;
+    use swc_common::sync::Lrc;
+    use swc_common::{
+        errors::{ColorConfig, Handler},
+        FileName, FilePathMapping, SourceMap,
+    };
+    use swc_ecma_ast::{Ident, JSXAttrName, ModuleItem};
+    use swc_ecma_codegen::{text_writer, Emitter};
+    use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
+    use swc_ecma_visit::{VisitAllWith, VisitMut, VisitMutWith};
+
+    pub fn prefix_classes(
+        /* source_file: &Path,*/ prefix: &str,
+        classnames: Vec<String>,
+    ) -> anyhow::Result<()> {
+        let cm: Lrc<SourceMap> = Default::default();
+        let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
+
+        // Real usage
+        // let fm = cm
+        //     .load_file(Path::new("test.js"))
+        //     .expect("failed to load test.js");
+        let fm = cm.new_source_file(
+            FileName::Custom("test.js".into()),
+            "function foo(arg) { return <div className=\"sr-only visible test\" intent=\"karma\" />}"
+                .into(),
+        );
+        let lexer = Lexer::new(
+            Syntax::Typescript(swc_ecma_parser::TsConfig {
+                tsx: true,
+                ..Default::default()
+            }),
+            // EsVersion defaults to es5
+            Default::default(),
+            StringInput::from(&*fm),
+            None,
+        );
+
+        let mut parser = Parser::new_from(lexer);
+
+        for e in parser.take_errors() {
+            e.into_diagnostic(&handler).emit();
+        }
+
+        let mut module = parser
+            .parse_module()
+            .map_err(|e| {
+                // Unrecoverable fatal error occurred
+                e.into_diagnostic(&handler).emit()
+            })
+            .expect("failed to parser module");
+
+        module.visit_mut_children_with(&mut PrependClassNames::new(prefix, classnames));
+
+        let mut output = Vec::new();
+        let writer = text_writer::JsWriter::new(cm.clone(), "\n", &mut output, None);
+
+        let mut emitter = swc_ecma_codegen::Emitter {
+            cfg: Default::default(),
+            cm: cm.clone(),
+            comments: None,
+            wr: Box::new(writer),
+        };
+
+        emitter
+            .emit_module(&module)
+            .context("failed to emit edit module")?;
+
+        println!(
+            "File edited and saved successfully -> \n {:?}",
+            String::from_utf8_lossy(&output)
+        );
+
+        Ok(())
+    }
+
+    struct PrependClassNames<'s> {
+        pub prefix: &'s str,
+        class_names: Vec<String>,
+    }
+
+    impl<'s> PrependClassNames<'s> {
+        fn new(prefix: &'s str, class_names: Vec<String>) -> Self {
+            Self {
+                prefix,
+                class_names,
+            }
+        }
+    }
+
+    impl<'s> VisitMut for PrependClassNames<'s> {
+        fn visit_mut_jsx_attr(&mut self, n: &mut swc_ecma_ast::JSXAttr) {
+            if let JSXAttrName::Ident(name) = &n.name {
+                let ident = &name.sym;
+                if ident.contains("class") || ident.contains("Class") {
+                    n.value.visit_mut_with(self);
+                }
+            }
+        }
+
+        fn visit_mut_str(&mut self, n: &mut swc_ecma_ast::Str) {
+            let replacements: Vec<_> = n
+                .value
+                .split(' ')
+                .map(|class| {
+                    let class = class.to_string();
+                    if self.class_names.contains(&class) {
+                        format!("{}{}", self.prefix, class)
+                    } else {
+                        class
+                    }
+                })
+                .collect();
+
+            let replacement = Atom::new(format!("\"{}\"", replacements.join(" ")));
+
+            n.raw = Some(replacement)
+        }
+    }
+}
