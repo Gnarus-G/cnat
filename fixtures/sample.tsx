4d402a493498c966a11c9cbd3be59596ad36c537
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-03-17 11:53:11 -0400

    chore: updating fixtures and snapshots

--- fixtures/sample.tsx
+++ fixtures/sample.tsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function Foo() {
   return (
     <div
-      className="sr-only visible untouched sm:[&_button]:text-sm"
+      className="sr-only visible untouched 
+          sm:[&_button]:text-sm"
       intent="karma"
     >
       <Button className="[&>.MuiButton-startIcon]:absolute">
